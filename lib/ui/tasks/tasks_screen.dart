f8bafc5c0a87b1416a90a0efdda96e8525e5eb34
mobarak <mobarak.ice.06@gmail.com>
2023-07-01 03:02:31 +0600

    Use context.selector instead of selector because whole widget need to rebuild when value get changes

--- lib/ui/tasks/tasks_screen.dart
+++ lib/ui/tasks/tasks_screen.dart
@@ -13,58 +13,56 @@ class TodoListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Selector<TodoListModel, List<Task>>(
-      selector: (_, model) => model.filteredTodos,
-      builder: (context, todos, _) {
-        return ListView.builder(
-          //key: key: TodoKeys.todoList,
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            final todo = todos[index];
+    var tasks = context
+        .select<TodoListModel, List<Task>>((model) => model.filteredTodos);
 
-            return Dismissible(
-              key: TodoKeys.todoItem(todo.id),
-              onDismissed: (_) => onRemove(context, todo),
-              child: ListTile(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) {
-                        return DetailsScreen(
-                          id: todo.id,
-                          onRemove: () {
-                            Navigator.pop(context);
-                            onRemove(context, todo);
-                          },
-                        );
+    return ListView.builder(
+      key: TodoKeys.todoList,
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        final todo = tasks.elementAt(index);
+
+        return Dismissible(
+          key: TodoKeys.todoItem(todo.id),
+          onDismissed: (_) => onRemove(context, todo),
+          child: ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) {
+                    return DetailsScreen(
+                      id: todo.id,
+                      onRemove: () {
+                        Navigator.pop(context);
+                        onRemove(context, todo);
                       },
-                    ),
-                  );
-                },
-                leading: Checkbox(
-                  key: TodoKeys.todoItemCheckbox(todo.id),
-                  value: todo.complete,
-                  onChanged: (complete) {
-                    Provider.of<TodoListModel>(context, listen: false)
-                        .updateTodo(todo.copy(complete: complete));
+                    );
                   },
                 ),
-                title: Text(
-                  todo.title,
-                  key: TodoKeys.todoItemTask(todo.id),
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                subtitle: Text(
-                  todo.description,
-                  key: TodoKeys.todoItemNote(todo.id),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-              ),
-            );
-          },
+              );
+            },
+            leading: Checkbox(
+              key: TodoKeys.todoItemCheckbox(todo.id),
+              value: todo.complete,
+              onChanged: (complete) {
+                Provider.of<TodoListModel>(context, listen: false)
+                    .updateTodo(todo.copy(complete: complete));
+              },
+            ),
+            title: Text(
+              todo.title,
+              key: TodoKeys.todoItemTask(todo.id),
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            subtitle: Text(
+              todo.description,
+              key: TodoKeys.todoItemNote(todo.id),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
         );
       },
     );
